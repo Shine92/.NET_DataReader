90d99e94756456076b23f64b668ae001ea930492
shaoming Huang <msn8971@gmail.com>
2017-09-13 22:47:19 +0800

    ASP.NET

---
+++ test0913_test01/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace test0913_test01 {
+    public partial class Default : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            string cnString = @"Server=.\SQLExpress;database=Northwind;uid=sa;password=P@ssw0rd";
+            SqlConnection cn = new SqlConnection(cnString);
+            cn.Open();
+            string ComText = "select * from Products";
+            SqlCommand cmd = new SqlCommand(ComText,cn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read()) {
+                Response.Write("產品名稱: " + dr["ProductName"] + "<br/>");
+                //Response.Write(dr["UnitsInStock"]);
+            }
+            cmd.Cancel();
+            dr.Close();
+            cn.Close();
+        }
+    }
+}
